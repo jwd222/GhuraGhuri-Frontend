f4d16f4db6931af37a98936851ead2a794aa9626
jwd222 <shahjawad@iut-dhaka.edu>
2022-04-21 15:59:13 +0600

    calculating distance between coordinates

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -29,6 +29,21 @@ function Home() {
       console.log(err);
     }
   };
+  const calculateDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 6371e3; // metres
+    const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+    const φ2 = lat2 * Math.PI/180;
+    const Δφ = (lat2-lat1) * Math.PI/180;
+    const Δλ = (lon2-lon1) * Math.PI/180;
+
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+              Math.cos(φ1) * Math.cos(φ2) *
+              Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    const d = R * c; // in metres
+    return d;
+  }
 
   useEffect(() => {
     setID(localStorage.getItem('currentID'));
@@ -78,9 +93,20 @@ function Home() {
   function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
   }
+
   navigator.geolocation.getCurrentPosition(success, error, options);
   console.log(`Currennt latitude is: ${currentLat}`);
   console.log(`Currennt longitude is: ${currentLng}`);
+
+  const pinCoordinates = pinData.map((result)=>({ 
+    latitude: result.lat,
+    longitude: result.lng,
+  }));
+
+  console.log(pinCoordinates[0])
+
+  // const distanceFromPoint = calculateDistance(currentLat, currentLng, pinData[0].lat, pinData[0].lng);
+  // console.log(distanceFromPoint);
   
 
   const getTopArticle = () => {
