7588832c5510a4de5607b0319915f042ad2e3241
jwd222 <shahjawad@iut-dhaka.edu>
2022-04-21 13:52:37 +0600

    fixing syntax for asking for location

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -34,7 +34,7 @@ function Home() {
     getPins();
 
   }, []);
-  console.log(pins[0]);
+  // console.log(pins);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -59,7 +59,7 @@ function Home() {
   }
 
   //get default browser location
-  var opt = {
+  const options = {
       enableHighAccuracy: true,
       timeout: 5000,
       maximumAge: 0,
@@ -68,12 +68,13 @@ function Home() {
       var crd = pos.coords;
       console.log('Your current position is:');
       console.log(`Latitude : ${crd.latitude}`);
-      console.log(`Longitude: ${crd.longitude}`);        console.log(`More or less ${crd.accuracy} meters.`);
+      console.log(`Longitude: ${crd.longitude}`);       
+      console.log(`More or less ${crd.accuracy} meters.`);
   }
   function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
   }
-  navigator.geolocation.getCurrentPosition(success, error, opt);
+  navigator.geolocation.getCurrentPosition(success, error, options);
   
 
   const getTopArticle = () => {
